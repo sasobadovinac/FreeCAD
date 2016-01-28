3c6711c79cbefe60d51f3939e79866dbebd0148a
Yorik van Havre <yorik@uncreated.net>
2016-01-28 15:50:49 -0200

    py3: Fixed stupid error made during manual merge

--- src/Gui/WidgetFactory.cpp
+++ src/Gui/WidgetFactory.cpp
@@ -1131,11 +1131,7 @@ Py::Object PyResource::value(const Py::Tuple& args)
 #endif
         break;
     default:
-#if PY_MAJOR_VERSION >= 3
-        item = Py::Unicode("");
-#else
         item = Py::String("");
-#endif
         break;
     }
 
