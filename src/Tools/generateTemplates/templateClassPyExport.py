yorikvanhavre/FreeCAD-old

py3: Ported src/Base to python3

src/Tools/generateTemplates/templateClassPyExport.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 # (c) 2006 Juergen Riegel 
 
-import template,os,sys
+from __future__ import print_function # this allows py2 to print(str1,str2) correctly
+from . import template
+import os,sys
 import generateBase.generateModel_Module
 import generateBase.generateTools
 
@@ -11,20 +13,28 @@ class TemplateClassPyExport (template.ModelTemplate):
 		#self.ParentNamespace = "Base"
 		#self.Namespace = "Base"
 		encoding = sys.getfilesystemencoding()
-		path = self.path.decode(encoding)
-		exportName = self.export.Name.decode(encoding)
-		dirname = self.dirname.decode(encoding)
-		print "TemplateClassPyExport",path + exportName
+		path = self.path
+		if hasattr(path,"decode"): # this is python2. Otherwise this is unicode already
+			path = path.decode(encoding)
+		exportName = self.export.Name
+		if hasattr(exportName,"decode"): # this is python2. Otherwise this is unicode already
+			exportName = exportName.decode(encoding)
+		dirname = self.dirname
+		if hasattr(dirname,"decode"): # this is python2. Otherwise this is unicode already
+			dirname = dirname.decode(encoding)
+		print("TemplateClassPyExport",path + exportName)
 		# Imp.cpp must not exist, neither in path nor in dirname
 		if(not os.path.exists(path + exportName + "Imp.cpp")):
 			if(not os.path.exists(dirname + exportName + "Imp.cpp")):
 				file = open(path + exportName + "Imp.cpp",'w')
 				generateBase.generateTools.replace(self.TemplateImplement,locals(),file)
-		file = open(path + exportName + ".cpp",'w')
+		file = open(path + exportName + ".cpp",'wb')
 		generateBase.generateTools.replace(self.TemplateModule,locals(),file)
-		file = open(path + exportName + ".h",'w')
+		file.close()
+		file = open(path + exportName + ".h",'wb')
 		generateBase.generateTools.replace(self.TemplateHeader,locals(),file)
 		#file.write( generateBase.generateTools.replace(self.Template,locals()))
+		file.close()
 
 	TemplateHeader = """
 // This file is generated by src/Tools/generateTemaplates/templateClassPyExport.py out of the XML file
@@ -101,8 +111,10 @@ public:
     static PyObject * number_subtract_handler (PyObject *self, PyObject *other);
     /// callback for the number_multiply_handler
     static PyObject * number_multiply_handler (PyObject *self, PyObject *other);
+#if PY_MAJOR_VERSION < 3
     /// callback for the number_divide_handler
     static PyObject * number_divide_handler (PyObject *self, PyObject *other);
+#endif
     /// callback for the number_remainder_handler
     static PyObject * number_remainder_handler (PyObject *self, PyObject *other);
     /// callback for the number_divmod_handler
@@ -129,18 +141,24 @@ public:
     static PyObject * number_xor_handler (PyObject *self, PyObject *other);
     /// callback for the number_or_handler
     static PyObject * number_or_handler (PyObject *self, PyObject *other);
+#if PY_MAJOR_VERSION < 3
     /// callback for the number_coerce_handler
     static int number_coerce_handler (PyObject **self, PyObject **other);
+#endif
     /// callback for the number_int_handler
     static PyObject * number_int_handler (PyObject *self);
+#if PY_MAJOR_VERSION < 3
     /// callback for the number_long_handler
     static PyObject * number_long_handler (PyObject *self);
+#endif
     /// callback for the number_float_handler
     static PyObject * number_float_handler (PyObject *self);
+#if PY_MAJOR_VERSION < 3
     /// callback for the number_oct_handler
     static PyObject * number_oct_handler (PyObject *self);
     /// callback for the number_hex_handler
     static PyObject * number_hex_handler (PyObject *self);
+#endif
     //@}
 -
 + if (self.export.Sequence):
@@ -202,8 +220,8 @@ public:
     PyObject *getCustomAttributes(const char* attr) const;
     /// setter for special attributes (e.g. dynamic ones)
     int setCustomAttributes(const char* attr, PyObject *obj);
-    PyObject *_getattr(char *attr);              // __getattr__ function
-    int _setattr(char *attr, PyObject *value);        // __setattr__ function
+    PyObject *_getattro(PyObject *attro);              // __getattr__ function
+    int _setattro(PyObject *attro, PyObject *value);   // __setattr__ function
 -
 
     /// getter for the object handled by this class
@@ -243,16 +261,15 @@ using namespace @self.export.Namespace@;
 
 /// Type structure of @self.export.Name@
 PyTypeObject @self.export.Name@::Type = {
-    PyObject_HEAD_INIT(&PyType_Type)
-    0,                                                /*ob_size*/
+    PyVarObject_HEAD_INIT(&PyType_Type,0)
     "@self.export.Namespace@.@self.export.Twin@",     /*tp_name*/
     sizeof(@self.export.Name@),                       /*tp_basicsize*/
     0,                                                /*tp_itemsize*/
     /* methods */
     PyDestructor,                                     /*tp_dealloc*/
     0,                                                /*tp_print*/
-    __getattr,                                        /*tp_getattr*/
-    __setattr,                                        /*tp_setattr*/
+    0,                                                /*tp_getattr*/
+    0,                                                /*tp_setattr*/
     0,                                                /*tp_compare*/
     __repr,                                           /*tp_repr*/
 + if (self.export.NumberProtocol):
@@ -269,20 +286,24 @@ PyTypeObject @self.export.Name@::Type = {
     0,                                                /*tp_hash*/
     0,                                                /*tp_call */
     0,                                                /*tp_str  */
-    0,                                                /*tp_getattro*/
-    0,                                                /*tp_setattro*/
+    __getattro,                                       /*tp_getattro*/
+    __setattro,                                       /*tp_setattro*/
     /* --- Functions to access object as input/output buffer ---------*/
     0,                                                /* tp_as_buffer */
     /* --- Flags to define presence of optional/expanded features */
+#if PY_MAJOR_VERSION >= 3
+    Py_TPFLAGS_BASETYPE|Py_TPFLAGS_DEFAULT,        /*tp_flags */
+#else
 + if (self.export.RichCompare and self.export.NumberProtocol):
-    Py_TPFLAGS_BASETYPE|Py_TPFLAGS_HAVE_CLASS|Py_TPFLAGS_HAVE_RICHCOMPARE|Py_TPFLAGS_CHECKTYPES,        /*tp_flags */
+    Py_TPFLAGS_BASETYPE|Py_TPFLAGS_DEFAULT|Py_TPFLAGS_HAVE_RICHCOMPARE|Py_TPFLAGS_CHECKTYPES,        /*tp_flags */
 = elif (self.export.RichCompare):
-    Py_TPFLAGS_BASETYPE|Py_TPFLAGS_HAVE_CLASS|Py_TPFLAGS_HAVE_RICHCOMPARE,        /*tp_flags */
+    Py_TPFLAGS_BASETYPE|Py_TPFLAGS_DEFAULT|Py_TPFLAGS_HAVE_RICHCOMPARE,        /*tp_flags */
 = elif (self.export.NumberProtocol):
-    Py_TPFLAGS_BASETYPE|Py_TPFLAGS_HAVE_CLASS|Py_TPFLAGS_CHECKTYPES,        /*tp_flags */
+    Py_TPFLAGS_BASETYPE|Py_TPFLAGS_DEFAULT|Py_TPFLAGS_CHECKTYPES,        /*tp_flags */
 = else:
-    Py_TPFLAGS_BASETYPE|Py_TPFLAGS_HAVE_CLASS,        /*tp_flags */
+    Py_TPFLAGS_BASETYPE|Py_TPFLAGS_DEFAULT,        /*tp_flags */
 -
+#endif
     "@self.export.Documentation.UserDocu.replace('\\n','\\\\n\\"\\n    \\"')@",           /*tp_doc */
     0,                                                /*tp_traverse */
     0,                                                /*tp_clear */
@@ -336,7 +357,9 @@ PyNumberMethods @self.export.Name@::Number[] = { {
     number_add_handler,
     number_subtract_handler,
     number_multiply_handler,
+#if PY_MAJOR_VERSION < 3
     number_divide_handler,
+#endif
     number_remainder_handler,
     number_divmod_handler,
     number_power_handler,
@@ -350,12 +373,20 @@ PyNumberMethods @self.export.Name@::Number[] = { {
     number_and_handler,
     number_xor_handler,
     number_or_handler,
+#if PY_MAJOR_VERSION < 3
     number_coerce_handler,
+#endif
     number_int_handler,
+#if PY_MAJOR_VERSION < 3
     number_long_handler,
+#else
+    0,
+#endif
     number_float_handler,
+#if PY_MAJOR_VERSION < 3
     number_oct_handler,
     number_hex_handler,
+#endif
     NULL
 } };
 -
@@ -641,8 +672,14 @@ PyObject *@self.export.Name@::_repr(void)
 //--------------------------------------------------------------------------
 // @self.export.Name@ Attributes
 //--------------------------------------------------------------------------
-PyObject *@self.export.Name@::_getattr(char *attr)				// __getattr__ function: note only need to handle new state
+PyObject *@self.export.Name@::_getattro(PyObject *attro)				// __getattr__ function: note only need to handle new state
 {
+    char *attr;
+#if PY_MAJOR_VERSION >= 3
+    attr = PyUnicode_AsUTF8(attro);
+#else
+    attr = PyString_AsString(attro);
+#endif
     try {
         // getter method for special Attributes (e.g. dynamic ones)
         PyObject *r = getCustomAttributes(attr);
@@ -662,7 +699,7 @@ PyObject *@self.export.Name@::_getattr(char *attr)				// __getattr__ function: n
     catch(const std::exception& e) // catch other c++ exceptions
     {
         std::string str;
-        str += "FC++ exception thrown (";
+        str += "C++ exception thrown (";
         str += e.what();
         str += ")";
         Base::Console().Error(str.c_str());
@@ -697,11 +734,11 @@ PyObject *@self.export.Name@::_getattr(char *attr)				// __getattr__ function: n
     }
 #endif  // DONT_CATCH_CXX_EXCEPTIONS
 
-    PyObject *rvalue = Py_FindMethod(Methods, this, attr);
+    PyObject *rvalue = PyObject_GenericGetAttr(this, attro);
     if (rvalue == NULL)
     {
         PyErr_Clear();
-        return @self.export.Father@::_getattr(attr);
+        return @self.export.Father@::_getattro(attro);
     }
     else
     {
@@ -709,8 +746,14 @@ PyObject *@self.export.Name@::_getattr(char *attr)				// __getattr__ function: n
     }
 }
 
-int @self.export.Name@::_setattr(char *attr, PyObject *value) 	// __setattr__ function: note only need to handle new state
+int @self.export.Name@::_setattro(PyObject *attro, PyObject *value) 	// __setattr__ function: note only need to handle new state
 {
+    char *attr;
+#if PY_MAJOR_VERSION >= 3
+    attr = PyUnicode_AsUTF8(attro);
+#else
+    attr = PyString_AsString(attro);
+#endif
     try {
         // setter for  special Attributes (e.g. dynamic ones)
         int r = setCustomAttributes(attr, value);
@@ -730,7 +773,7 @@ int @self.export.Name@::_setattr(char *attr, PyObject *value) 	// __setattr__ fu
     catch(const std::exception& e) // catch other c++ exceptions
     {
         std::string str;
-        str += "FC++ exception thrown (";
+        str += "C++ exception thrown (";
         str += e.what();
         str += ")";
         Base::Console().Error(str.c_str());
@@ -765,7 +808,7 @@ int @self.export.Name@::_setattr(char *attr, PyObject *value) 	// __setattr__ fu
     }
 #endif  // DONT_CATCH_CXX_EXCEPTIONS
 
-    return @self.export.Father@::_setattr(attr, value);
+    return @self.export.Father@::_setattro(attro, value);
 }
 -
 

sasobadovinac/FreeCAD

src/Tools/generateTemplates/templateClassPyExport.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 # (c) 2006 Juergen Riegel 
 
-import template,os,sys
+from __future__ import print_function # this allows py2 to print(str1,str2) correctly
+from . import template
+import os,sys
 import generateBase.generateModel_Module
 import generateBase.generateTools
 
@@ -11,20 +13,28 @@ class TemplateClassPyExport (template.ModelTemplate):
 		#self.ParentNamespace = "Base"
 		#self.Namespace = "Base"
 		encoding = sys.getfilesystemencoding()
-		path = self.path.decode(encoding)
-		exportName = self.export.Name.decode(encoding)
-		dirname = self.dirname.decode(encoding)
-		print "TemplateClassPyExport",path + exportName
+		path = self.path
+		if hasattr(path,"decode"): # this is python2. Otherwise this is unicode already
+			path = path.decode(encoding)
+		exportName = self.export.Name
+		if hasattr(exportName,"decode"): # this is python2. Otherwise this is unicode already
+			exportName = exportName.decode(encoding)
+		dirname = self.dirname
+		if hasattr(dirname,"decode"): # this is python2. Otherwise this is unicode already
+			dirname = dirname.decode(encoding)
+		print("TemplateClassPyExport",path + exportName)
 		# Imp.cpp must not exist, neither in path nor in dirname
 		if(not os.path.exists(path + exportName + "Imp.cpp")):
 			if(not os.path.exists(dirname + exportName + "Imp.cpp")):
 				file = open(path + exportName + "Imp.cpp",'w')
 				generateBase.generateTools.replace(self.TemplateImplement,locals(),file)
-		file = open(path + exportName + ".cpp",'w')
+		file = open(path + exportName + ".cpp",'wb')
 		generateBase.generateTools.replace(self.TemplateModule,locals(),file)
-		file = open(path + exportName + ".h",'w')
+		file.close()
+		file = open(path + exportName + ".h",'wb')
 		generateBase.generateTools.replace(self.TemplateHeader,locals(),file)
 		#file.write( generateBase.generateTools.replace(self.Template,locals()))
+		file.close()
 
 	TemplateHeader = """
 // This file is generated by src/Tools/generateTemaplates/templateClassPyExport.py out of the XML file
@@ -104,8 +114,10 @@ public:
     static PyObject * number_subtract_handler (PyObject *self, PyObject *other);
     /// callback for the number_multiply_handler
     static PyObject * number_multiply_handler (PyObject *self, PyObject *other);
+#if PY_MAJOR_VERSION < 3
     /// callback for the number_divide_handler
     static PyObject * number_divide_handler (PyObject *self, PyObject *other);
+#endif
     /// callback for the number_remainder_handler
     static PyObject * number_remainder_handler (PyObject *self, PyObject *other);
     /// callback for the number_divmod_handler
@@ -132,18 +144,24 @@ public:
     static PyObject * number_xor_handler (PyObject *self, PyObject *other);
     /// callback for the number_or_handler
     static PyObject * number_or_handler (PyObject *self, PyObject *other);
+#if PY_MAJOR_VERSION < 3
     /// callback for the number_coerce_handler
     static int number_coerce_handler (PyObject **self, PyObject **other);
+#endif
     /// callback for the number_int_handler
     static PyObject * number_int_handler (PyObject *self);
+#if PY_MAJOR_VERSION < 3
     /// callback for the number_long_handler
     static PyObject * number_long_handler (PyObject *self);
+#endif
     /// callback for the number_float_handler
     static PyObject * number_float_handler (PyObject *self);
+#if PY_MAJOR_VERSION < 3
     /// callback for the number_oct_handler
     static PyObject * number_oct_handler (PyObject *self);
     /// callback for the number_hex_handler
     static PyObject * number_hex_handler (PyObject *self);
+#endif
     //@}
 -
 + if (self.export.Sequence):
@@ -206,8 +224,8 @@ public:
     /// setter for special attributes (e.g. dynamic ones)
     /// Output: Success=1, Failure=-1, Ignore=0
     int setCustomAttributes(const char* attr, PyObject *obj);
-    PyObject *_getattr(char *attr);              // __getattr__ function
-    int _setattr(char *attr, PyObject *value);        // __setattr__ function
+    PyObject *_getattro(PyObject *attro);              // __getattr__ function
+    int _setattro(PyObject *attro, PyObject *value);   // __setattr__ function
 -
 
     /// getter for the object handled by this class
@@ -245,16 +263,15 @@ using namespace @self.export.Namespace@;
 
 /// Type structure of @self.export.Name@
 PyTypeObject @self.export.Name@::Type = {
-    PyObject_HEAD_INIT(&PyType_Type)
-    0,                                                /*ob_size*/
+    PyVarObject_HEAD_INIT(&PyType_Type,0)
     "@self.export.Namespace@.@self.export.Twin@",     /*tp_name*/
     sizeof(@self.export.Name@),                       /*tp_basicsize*/
     0,                                                /*tp_itemsize*/
     /* methods */
     PyDestructor,                                     /*tp_dealloc*/
     0,                                                /*tp_print*/
-    __getattr,                                        /*tp_getattr*/
-    __setattr,                                        /*tp_setattr*/
+    0,                                                /*tp_getattr*/
+    0,                                                /*tp_setattr*/
     0,                                                /*tp_compare*/
     __repr,                                           /*tp_repr*/
 + if (self.export.NumberProtocol):
@@ -271,20 +288,24 @@ PyTypeObject @self.export.Name@::Type = {
     0,                                                /*tp_hash*/
     0,                                                /*tp_call */
     0,                                                /*tp_str  */
-    0,                                                /*tp_getattro*/
-    0,                                                /*tp_setattro*/
+    __getattro,                                       /*tp_getattro*/
+    __setattro,                                       /*tp_setattro*/
     /* --- Functions to access object as input/output buffer ---------*/
     0,                                                /* tp_as_buffer */
     /* --- Flags to define presence of optional/expanded features */
+#if PY_MAJOR_VERSION >= 3
+    Py_TPFLAGS_BASETYPE|Py_TPFLAGS_DEFAULT,        /*tp_flags */
+#else
 + if (self.export.RichCompare and self.export.NumberProtocol):
-    Py_TPFLAGS_HAVE_CLASS|Py_TPFLAGS_HAVE_RICHCOMPARE|Py_TPFLAGS_CHECKTYPES,        /*tp_flags */
+    Py_TPFLAGS_DEFAULT|Py_TPFLAGS_HAVE_RICHCOMPARE|Py_TPFLAGS_CHECKTYPES,        /*tp_flags */
 = elif (self.export.RichCompare):
-    Py_TPFLAGS_HAVE_CLASS|Py_TPFLAGS_HAVE_RICHCOMPARE,        /*tp_flags */
+    Py_TPFLAGS_DEFAULT|Py_TPFLAGS_HAVE_RICHCOMPARE,        /*tp_flags */
 = elif (self.export.NumberProtocol):
-    Py_TPFLAGS_HAVE_CLASS|Py_TPFLAGS_CHECKTYPES,        /*tp_flags */
+    Py_TPFLAGS_DEFAULT|Py_TPFLAGS_CHECKTYPES,        /*tp_flags */
 = else:
-    Py_TPFLAGS_HAVE_CLASS,        /*tp_flags */
+    Py_TPFLAGS_DEFAULT,        /*tp_flags */
 -
+#endif
     "@self.export.Documentation.UserDocu.replace('\\n','\\\\n\\"\\n    \\"')@",           /*tp_doc */
     0,                                                /*tp_traverse */
     0,                                                /*tp_clear */
@@ -339,7 +360,9 @@ PyNumberMethods @self.export.Name@::Number[] = { {
     number_add_handler,
     number_subtract_handler,
     number_multiply_handler,
+#if PY_MAJOR_VERSION < 3
     number_divide_handler,
+#endif
     number_remainder_handler,
     number_divmod_handler,
     number_power_handler,
@@ -353,12 +376,20 @@ PyNumberMethods @self.export.Name@::Number[] = { {
     number_and_handler,
     number_xor_handler,
     number_or_handler,
+#if PY_MAJOR_VERSION < 3
     number_coerce_handler,
+#endif
     number_int_handler,
+#if PY_MAJOR_VERSION < 3
     number_long_handler,
+#else
+    0,
+#endif
     number_float_handler,
+#if PY_MAJOR_VERSION < 3
     number_oct_handler,
     number_hex_handler,
+#endif
     NULL,
     NULL,
     NULL,
@@ -662,8 +693,14 @@ PyObject *@self.export.Name@::_repr(void)
 //--------------------------------------------------------------------------
 // @self.export.Name@ Attributes
 //--------------------------------------------------------------------------
-PyObject *@self.export.Name@::_getattr(char *attr)				// __getattr__ function: note only need to handle new state
+PyObject *@self.export.Name@::_getattro(PyObject *attro)				// __getattr__ function: note only need to handle new state
 {
+    char *attr;
+#if PY_MAJOR_VERSION >= 3
+    attr = PyUnicode_AsUTF8(attro);
+#else
+    attr = PyString_AsString(attro);
+#endif
     try {
         // getter method for special Attributes (e.g. dynamic ones)
         PyObject *r = getCustomAttributes(attr);
@@ -683,7 +720,7 @@ PyObject *@self.export.Name@::_getattr(char *attr)				// __getattr__ function: n
     catch(const std::exception& e) // catch other c++ exceptions
     {
         std::string str;
-        str += "FC++ exception thrown (";
+        str += "C++ exception thrown (";
         str += e.what();
         str += ")";
         Base::Console().Error(str.c_str());
@@ -718,11 +755,11 @@ PyObject *@self.export.Name@::_getattr(char *attr)				// __getattr__ function: n
     }
 #endif  // DONT_CATCH_CXX_EXCEPTIONS
 
-    PyObject *rvalue = Py_FindMethod(Methods, this, attr);
+    PyObject *rvalue = PyObject_GenericGetAttr(this, attro);
     if (rvalue == NULL)
     {
         PyErr_Clear();
-        return @self.export.Father@::_getattr(attr);
+        return @self.export.Father@::_getattro(attro);
     }
     else
     {
@@ -730,8 +767,15 @@ PyObject *@self.export.Name@::_getattr(char *attr)				// __getattr__ function: n
     }
 }
 
-int @self.export.Name@::_setattr(char *attr, PyObject *value) // __setattr__ function: note only need to handle new state
+int @self.export.Name@::_setattro(PyObject *attro, PyObject *value) 	// __setattr__ function: note only need to handle new state
+
 {
+    char *attr;
+#if PY_MAJOR_VERSION >= 3
+    attr = PyUnicode_AsUTF8(attro);
+#else
+    attr = PyString_AsString(attro);
+#endif
     try {
         // setter for  special Attributes (e.g. dynamic ones)
         int r = setCustomAttributes(attr, value);
@@ -757,7 +801,7 @@ int @self.export.Name@::_setattr(char *attr, PyObject *value) // __setattr__ fun
     catch(const std::exception& e) // catch other c++ exceptions
     {
         std::string str;
-        str += "FC++ exception thrown (";
+        str += "C++ exception thrown (";
         str += e.what();
         str += ")";
         Base::Console().Error(str.c_str());
@@ -792,7 +836,7 @@ int @self.export.Name@::_setattr(char *attr, PyObject *value) // __setattr__ fun
     }
 #endif  // DONT_CATCH_CXX_EXCEPTIONS
 
-    return @self.export.Father@::_setattr(attr, value);
+    return @self.export.Father@::_setattro(attro, value);
 }
 -
 